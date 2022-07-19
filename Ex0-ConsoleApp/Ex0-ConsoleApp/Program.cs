dc8756a90fdea7180dc343f0044814d3e2ccd2b6
Yineng Li <yineng.li@outlook.com>
2022-07-18 18:41:18 -0700

    public test

---
+++ Ex0-ConsoleApp/Ex0-ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex0_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            string name = Console.ReadLine();
+            Console.WriteLine("changed");
+        }
+    }
+}
